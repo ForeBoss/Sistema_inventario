12396475272aebaabbc8d942803afb533ce4502b
Andres <foreroandres8911@gmail.com>
2023-11-02 21:32:30 -0500

    Proyecto 1

---
+++ src/main/java/com/software_inventario/SoftwareParaGestionDeInventarioApplication.java
@@ -0,0 +1,13 @@
+package com.software_inventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareParaGestionDeInventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareParaGestionDeInventarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/software_inventario/SoftwareParaGestionDeInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software_inventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareParaGestionDeInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
